67930f30e8b33d5ed893439c7ce3802187a8691a
Brock Callahan <jbrockc@gmail.com>
2021-10-21 19:48:30 -0500

    Remove mention of semantic-ui
    I'm not longer using it.

--- src/components/SiteInfo.jsx
+++ src/components/SiteInfo.jsx
@@ -1,9 +1,7 @@
 function SiteInfo() {
     return (
         <section className="prose prose-yellow text-white">
-            <p>This site was built with <a href="http://reactjs.com">ReactJS</a>
-                <span>, </span>
-                <a href="https://react.semantic-ui.com/">SemanticUI</a> and <a href="https://tailwindcss.com">tailwindcss</a>.</p>
+            <p>This site was built with <a href="http://reactjs.com">ReactJS</a> and <a href="https://tailwindcss.com">tailwindcss</a>.</p>
         </section>
     );
 }
