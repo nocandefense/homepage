3398b57eb89c8278fb90e440c720f7f65bbb9e8c
Brock Callahan <jbrockc@gmail.com>
2021-10-21 23:52:21 -0500

    Make small edits to HTML

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 function Main({ children }) {
     return (
         <section className="bg-black backdrop-filter backdrop-opacity-10 p-5 shadow-sm rounded-md">
-            <p>Welcome to my homepage</p>
+            <h3>Welcome to my homepage</h3>
             <p>I was working on a project with React.js, which is a JavaScript UI library, so I decided to
                 rebuild my homepage with it.</p>
             {children}
