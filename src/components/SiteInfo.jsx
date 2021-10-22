11b6b0e21da3d3f964d25fa3c386e8c3c04654a3
Brock Callahan <jbrockc@gmail.com>
2021-10-21 20:00:34 -0500

    Add to site info

--- src/components/SiteInfo.jsx
+++ src/components/SiteInfo.jsx
@@ -1,7 +1,7 @@
 function SiteInfo() {
     return (
         <section className="prose prose-yellow text-white">
-            <p>This site was built with <a href="http://reactjs.com">ReactJS</a> and <a href="https://tailwindcss.com">tailwindcss</a>.</p>
+            <p>This site was built with <a href="http://reactjs.com">ReactJS</a> and <a href="https://tailwindcss.com">tailwindcss</a>. React is almost certainly overkill for a personal website. But maybe it will become more useful as I add more stuff here.</p>
         </section>
     );
 }
