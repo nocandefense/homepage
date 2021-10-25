6acd910fefea275c184f8ddb42f05564e678fb38
Brock Callahan <jbrockc@gmail.com>
2021-10-25 13:38:21 -0500

    Remove a comment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,6 @@ function Home() {
         <div>
             <h2>Home</h2>
             <h3>Welcome to my homepage</h3>
-            {/* <p>I was working on a project with React.js, which is a JavaScript UI library, so I decided to
-                rebuild my homepage with it.</p> */}
         </div>
     )
 }
