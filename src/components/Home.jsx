70f635334aae9ff885518b671436c1b57c676613
Brock Callahan <jbrockc@gmail.com>
2021-10-31 02:32:39 -0500

    Add under-construction message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
         <div>
             <h2>Home</h2>
             <h3>Welcome to my homepage</h3>
-
+            <p>This page is under construction.</p>
         </div>
     )
 }
