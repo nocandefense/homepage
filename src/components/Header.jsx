7bacc623ce8df117e4989a8f5d1f53b8c5dd0378
Brock Callahan <jbrockc@gmail.com>
2021-11-20 07:52:32 -0600

    center page title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function Header({ children }) {
     return (
-        <header className="align-middle">
-            <Link to="/"><h1 title="brockcallahan.com" className="text-white text-5xl text-center font-tech scale-90 backdrop-hue-rotate-15 backdrop-invert">brockcallahan.com</h1></Link>
+        <header className="grid place-items-center h-screen">
+            <Link to="/"><h1 title="brockcallahan.com" className="text-white text-5xl font-tech animate-bounce">brockcallahan.com</h1></Link>
             {children}
         </header>
     );
