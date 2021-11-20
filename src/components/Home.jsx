63d3036a075f643a611907a44a691f96a300897e
Brock Callahan <jbrockc@gmail.com>
2021-11-20 05:19:24 -0600

    remove some stuff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,6 @@
 function Home() {
     return (
         <div>
-            <h2>Home</h2>
-            <h3>Welcome to my homepage</h3>
             <p>This page is under construction.</p>
         </div>
     )
