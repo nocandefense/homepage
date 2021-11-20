3125c780af33240906d8265ded9ac2e5015fe544
Brock Callahan <jbrockc@gmail.com>
2021-11-20 07:55:21 -0600

    remove unused fonts from head

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=IBM+Plex+Mono:wght@200&family=Press+Start+2P&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@200&display=swap" rel="stylesheet" />
       </Helmet>
       <div className="App h-full lg:w-3/6 p-4 mx-auto">
         <Header />
